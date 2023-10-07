833ab4bb5fd05dc4b48650d7df1e37854386845f
ayodya <ayodyakcc@gmail.com>
2023-10-07 19:40:03 +0530

    mail

--- contactform/tailormade.php
+++ contactform/tailormade.php
@@ -1,131 +1,126 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+?>
+
+<?php
+
 if (isset($_POST['submit'])) {
 
-    $to = 'ayodyakcc@gmail.com';
+
+
+    $mail = new PHPMailer(true); // Create a new PHPMailer instance
+
+    // SMTP Configuration
+    $mail->isSMTP(); // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com'; // Your SMTP server address
+    $mail->SMTPAuth = true; // Enable SMTP authentication
+    $mail->Username = 'ayodyakcc@gmail.com'; // Your SMTP username
+    $mail->Password = 'vzsy oqxj dhwy wvji'; // Your SMTP password
+    $mail->SMTPSecure = 'tls'; // Enable TLS encryption; 'ssl' is also possible
+    $mail->Port = 587; // TCP port to connect to
+
+
+
+
+
+
+    // Set email parameters
+    $to = 'ayodyakcc@gmail.com'; // Replace with your recipient email address
     $subject = 'New Customized Tour Request';
 
+    // Build the email message
+    $message = '';
 
+    // Collect and append form data to the message
 
-    $headers = "From: " . strip_tags($_POST["email"]) . "\r\n";
-    $headers .= "Reply-To: " . strip_tags($_POST["email"]) . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    // Travel method
+    $message .= '<p><b>Wie wollen Sie reisen?</b>: ' . $_POST['travel-method'] . '</p>';
 
-    $message = '<p><b>Customized Tour Request From ';
+    // Accommodation type
+    $message .= '<p><b>Welche Art der Unterkunft wünschen Sie?</b>: ' . $_POST['accommodation-type'] . '</p>';
 
-    if (!empty($_POST['name'])) {
-        $name = $_POST["name"];
-        $message = $message . $name . '</b><br>';
-    }
+    // Planning progress
+    $message .= '<p><b>Wie weit sind Sie mit Ihrer Reiseplanung?</b>: ' . $_POST['planning-progress'] . '</p>';
 
-    $message = $message . '<p><b>Language Selection:</b></p>';
-
-
+    // Languages
     if (!empty($_POST['language'])) {
-        // $languages = $_POST["language"];
+        $message .= '<p><b>Sprachen:</b></p>';
         foreach ($_POST['language'] as $value) {
-            // echo $value . '<br>';
-            $message = $message . $value . '<br>';
-
+            $message .= '<p>' . $value . '</p>';
         }
-
     }
 
-
+    // Other language
     if (!empty($_POST['other-language'])) {
-        $otherLang = $_POST["other-language"];
-        // echo $otherLang;
-        $message = $message . $otherLang . '<br>';
+        $message .= '<p><b>Andere Sprache:</b>: ' . $_POST['other-language'] . '</p>';
     }
 
-    $message = $message . '<p><b>Vacation Type:</b></p>';
-
+    // Vacation type
     if (!empty($_POST['vacation-type'])) {
-        // $vacationType = $_POST["vacation-type"];
+        $message .= '<p><b>Art des Urlaubs, den ich suche:</b></p>';
         foreach ($_POST['vacation-type'] as $value) {
-            // echo $value . '<br>';
-            $message = $message . $value . '<br>';
+            $message .= '<p>' . $value . '</p>';
         }
     }
 
-
-    $message = $message . '<p><b>Accommodation Type:</b></p>';
-
-
-    if (!empty($_POST['accommodation'])) {
-        $accommodation = $_POST["accommodation"];
-        // echo $accommodation . '<br>';
-        $message = $message . $accommodation . '<br>';
-    }
-
-
-
-    $message = $message . '<p><b>Additional Services:</b></p><br>';
-
+    // Additional services
     if (!empty($_POST['additional'])) {
-        // $additional = $_POST["additional"];
+        $message .= '<p><b>Zusätzliche Dienstleistungen, die ich benötige:</b></p>';
         foreach ($_POST['additional'] as $value) {
-            // echo $value . '<br>';
-            $message = $message . $value . '<br>';
+            $message .= '<p>' . $value . '</p>';
         }
     }
 
-
-    $message = $message . '<p><b>Message:</b></p>';
-
-
+    // User message
     if (!empty($_POST['user-message'])) {
-        $userMessage = $_POST["user-message"];
-        $message = $message . $userMessage . '<br>';
+        $message .= '<p><b>Meine zusätzlichen Anforderungen:</b></p>';
+        $message .= '<p>' . $_POST['user-message'] . '</p>';
     }
 
+    // Adults
+    $message .= '<p><b>Erwachsene:</b>: ' . $_POST['adults'] . '</p>';
 
+    // Children
+    $message .= '<p><b>Kinder:</b>: ' . $_POST['children'] . '</p>';
 
-    $message = $message . '<p><b>Adults:</b></p>';
-    if (!empty($_POST['adults'])) {
-        $adults = $_POST["adults"];
-        $message = $message . $adults . '<br>';
+    // Start date
+    $message .= '<p><b>Startdatum:</b>: ' . $_POST['start'] . '</p>';
+
+    // End date
+    $message .= '<p><b>Enddatum:</b>: ' . $_POST['end'] . '</p>';
+
+    // Flight number
+    $message .= '<p><b>Flugnummer:</b>: ' . $_POST['flight'] . '</p>';
+
+    // Name
+    $message .= '<p><b>Name:</b>: ' . $_POST['name'] . '</p>';
+
+    // Email
+    $message .= '<p><b>Email:</b>: ' . $_POST['email'] . '</p>';
+
+    // Set the email body and content type
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+
+    // Add recipient and sender email addresses
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    $mail->addAddress($to);
+
+    try {
+        // Send the email
+        if ($mail->send()) {
+            header("Location: /thank-you");
+        } else {
+            echo "Email could not be sent. Error: " . $mail->ErrorInfo;
+        }
+    } catch (Exception $e) {
+        echo "Email could not be sent. Error: " . $mail->ErrorInfo;
     }
-
-
-    $message = $message . '<p><b>Children:</b></p>';
-    if (!empty($_POST['children'])) {
-        $children = $_POST["children"];
-        $message = $message . $children . '<br>';
-    }
-
-
-    $message = $message . '<p><b>Start Date:</b></p>';
-    if (!empty($_POST['start'])) {
-        $start = $_POST["start"];
-        $message = $message . $start . '<br>';
-    }
-
-
-    $message = $message . '<p><b>End Date:</b></p>';
-    if (!empty($_POST['end'])) {
-        $end = $_POST["end"];
-        $message = $message . $end . '<br>';
-    }
-
-
-    $message = $message . '<p><b>Flight Number:</b></p>';
-    if (!empty($_POST['flight'])) {
-        $flight = $_POST["flight"];
-        $message = $message . $flight . '<br>';
-    }
-
-    $message = $message . '<p><b>Email:</b></p>';
-
-    if (!empty($_POST['email'])) {
-        $email = $_POST["email"];
-        $message = $message . $email . '<br>';
-    }
-
-    // echo $message;
-
-    mail($to, $subject, $message, $headers);
-
-    header("Location: " . "/thank-you");
 }
 ?>
